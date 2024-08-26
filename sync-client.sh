8f7464062666b93cad76f01f1922ff3bee4a7419
루밀LuMir <rpfos@naver.com>
2024-08-26 20:27:12 +0900

    feat: create sync-client.sh

---
+++ sync-client.sh
@@ -0,0 +1,27 @@
+# .github/workflows
+mkdir -p .github/workflows
+cp configs/.github/workflows/lint.yml .github/workflows
+
+# .github
+mkdir -p .github
+cp configs/.github/dependabot.yml .github
+
+# .husky
+mkdir -p .husky
+cp configs/.husky/pre-commit .husky
+
+# .vscode
+mkdir -p .vscode
+cp configs/.vscode/settings.json .vscode
+
+# root
+cp configs/.editorconfig .
+cp configs/.eslintignore .
+cp configs/.eslintrc.js .
+cp configs/.markdownlint.json .
+cp configs/.markdownlintignore .
+cp configs/.nvmrc .
+cp configs/.prettierignore .
+cp configs/.prettierrc.js .
+cp configs/babel.config.js .
+cp configs/VScode.code-workspace .
