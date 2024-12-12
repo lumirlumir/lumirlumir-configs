9fe1a3131c58e56e0ff06cefe3e99add80ac9400
루밀LuMir <rpfos@naver.com>
2024-12-12 13:51:11 +0900

    refactor: refactor `index.js` for improved readability and structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+/**
+ * @fileoverview Main script file for the GitHub Actions `sync-server` action. This script creates a single YML file from multiple YML files.
+ */
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const fs = require('node:fs');
+const path = require('node:path');
 const yml = require('yaml');
 
-const userName = 'lumirlumir';
-const repositoryName = 'lumirlumir-configs';
+// --------------------------------------------------------------------------------
+// Declaration
+// --------------------------------------------------------------------------------
+
+const USER_NAME = 'lumirlumir';
+const REPOSITORY_NAME = 'lumirlumir-configs';
 const clientsDirPath = path.resolve(__dirname, '..', 'clients');
 const outputYmlFilePath = path.resolve(__dirname, '..', '.github', 'sync-server.yml');
 const inputYmlFilePaths = fs
@@ -17,11 +29,11 @@ const createYmlFile = () => {
     const ymlFileParsed = yml.parse(fs.readFileSync(inputYmlFilePath, 'utf-8'));
 
     const ymlFileParsedProcessed = {
-      [`${userName}/${path.parse(inputYmlFilePath).name}`]: ymlFileParsed[
-        `${userName}/${repositoryName}`
-      ].map(obj => ({
-        source: obj.dest,
-        dest: obj.source,
+      [`${USER_NAME}/${path.parse(inputYmlFilePath).name}`]: ymlFileParsed[
+        `${USER_NAME}/${REPOSITORY_NAME}`
+      ].map(({ dest, source }) => ({
+        source: dest,
+        dest: source,
       })),
     };
 
@@ -32,10 +44,15 @@ const createYmlFile = () => {
     // console.log(yml.stringify(ymlFileParsedProcessed));
     // console.log('\n-----\n');
 
-    outputYmlFile = `${outputYmlFile}${yml.stringify(ymlFileParsedProcessed)}\n`;
+    // Should use LF line endings because every file in the repository uses LF line endings due to the `.editorconfig` file.
+    outputYmlFile += `${yml.stringify(ymlFileParsedProcessed)}\n`;
   });
 
   return outputYmlFile;
 };
+
+// --------------------------------------------------------------------------------
+// Execution
+// --------------------------------------------------------------------------------
 
 fs.writeFileSync(outputYmlFilePath, createYmlFile(), 'utf-8');
