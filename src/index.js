142f5a6b5036a083d87617594b615faa3b3dacb1
루밀LuMir <rpfos@naver.com>
2024-12-12 14:33:05 +0900

    feat: add debug mode to `sync-server` action for enhanced logging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 /**
  * @fileoverview Main script file for the GitHub Actions `sync-server` action. This script creates a single YML file from multiple YML files.
+ *
+ * run: `node src`
+ * run(debug mode): `node src -d` or `node src --debug`
  */
 
 // --------------------------------------------------------------------------------
@@ -8,6 +11,7 @@
 
 const fs = require('node:fs');
 const path = require('node:path');
+const { log } = require('node:console');
 const yml = require('yaml');
 
 // --------------------------------------------------------------------------------
@@ -21,6 +25,7 @@ const outputYmlFilePath = path.resolve(__dirname, '..', '.github', 'sync-server.
 const inputYmlFilePaths = fs
   .readdirSync(clientsDirPath)
   .map(filePath => path.resolve(clientsDirPath, filePath));
+const secondArgv = process.argv[2];
 
 const createYmlFile = () => {
   let outputYmlFile = '';
@@ -37,15 +42,23 @@ const createYmlFile = () => {
       })),
     };
 
-    // console.log(ymlFileParsed);
-    // console.log('\n-----\n');
-    // console.log(ymlFileParsedProcessed);
-    // console.log('\n-----\n');
-    // console.log(yml.stringify(ymlFileParsedProcessed));
-    // console.log('\n-----\n');
-
     // Should use LF line endings because every file in the repository uses LF line endings due to the `.editorconfig` file.
     outputYmlFile += `${yml.stringify(ymlFileParsedProcessed)}\n`;
+
+    // Debug mode
+    if (secondArgv === '-d' || secondArgv === '--debug') {
+      log('inputYmlFilePath:');
+      log(inputYmlFilePath);
+      log('----------------------------------------------------------------------');
+      log('ymlFileParsed:');
+      log(ymlFileParsed);
+      log('----------------------------------------------------------------------');
+      log('ymlFileParsedProcessed:');
+      log(ymlFileParsedProcessed);
+      log('----------------------------------------------------------------------');
+      log('yml.stringify(ymlFileParsedProcessed):');
+      log(yml.stringify(ymlFileParsedProcessed));
+    }
   });
 
   return outputYmlFile;
