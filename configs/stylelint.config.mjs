abc29f19f5b12d2c8b019bc027b549dc37ace705
루밀LuMir <rpfos@naver.com>
2025-07-11 01:32:42 +0900

    chore(sync-client): synced file(s) with lumirlumir/web-moing.lumir.page (#293)
    synced local file(s) with
    [lumirlumir/web-moing.lumir.page](https://github.com/lumirlumir/web-moing.lumir.page).
    
    @lumirlumir check out
    [`7606b9f`](https://github.com/lumirlumir/web-moing.lumir.page/commit/7606b9f36f030cea900db0d314ccbb729fd80e45).🐣
    
    <details>
    <summary>Changed files</summary>
    <ul>
    <li>synced local <code>./clients/web-moing.lumir.page.yml</code> with
    remote <code>./.github/sync-client.yml</code></li>
    </ul>
    </details>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#16200460282](https://github.com/lumirlumir/web-moing.lumir.page/actions/runs/16200460282)

--- configs/stylelint.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
-  rules: {
-    'scss/no-global-function-names': null,
-  },
-};
