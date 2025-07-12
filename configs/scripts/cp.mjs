3b940633c0cfaecaa28339d5ec6e61905a96254e
lumirlumir <rpfos@naver.com>
2025-07-12 17:10:52 +0000

    chore(sync-client): synced local './configs/scripts/cp.mjs' with remote './scripts/cp.mjs'

--- configs/scripts/cp.mjs
+++ configs/scripts/cp.mjs
@@ -19,5 +19,6 @@ const args = process.argv.slice(2);
 for (let i = 0; i < args.length; i += 2) {
   cpSync(resolve(process.cwd(), args[i]), resolve(process.cwd(), args[i + 1]), {
     force: true,
+    recursive: true,
   });
 }
